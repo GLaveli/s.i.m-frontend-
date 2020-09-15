14805801e6ad8905a7f4b786855146083c79e4a4
unknown <g.laveli.p@gmail.com>
2020-09-15 10:58:19 -0300

    troca de IP

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  //baseURL: 'https://sim-solutions.herokuapp.com/',
-  baseURL: 'http://localhost:3001/',
+  baseURL: 'https://sim-solutions.herokuapp.com/',
+  //baseURL: 'http://localhost:3001/',
 });
 
 export default api;
